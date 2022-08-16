ec0728b75e9e3df236ad150ef66fb95f61447b1d
Marcos César Domingues <marcos.domingues@gcbinvestimentos.com>
2022-08-16 11:31:20 -0300

    Migrando Storybook para versão 6.x

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,15 +1,27 @@
 import { withKnobs, text } from '@storybook/addon-knobs'
+import { ComponentMeta, Story } from '@storybook/react'
 import Main from '.'
+import React from 'react'
 
 export default {
   title: 'Main',
   component: 'Main',
   decorators: [withKnobs]
-}
+} as unknown as ComponentMeta<typeof Main>
 
-export const Basic = () => (
+export const Basic: Story = () => (
   <Main
     title={text('Title', 'React Avançado')}
+    description={text(
+      'Description',
+      'TypeScript, ReactJS, NextJS e Styled Components'
+    )}
+  />
+)
+
+export const Secondary: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado123')}
     description={text(
       'Description',
       'TypeScript, ReactJS, NextJS e Styled Components'
