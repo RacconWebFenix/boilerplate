b110891e415df58945d45530d1ae6289358f4c83
Marcos César Domingues <marcos.domingues@gcbinvestimentos.com>
2022-09-05 08:29:30 -0300

    25. Usando Storybook Essentials e Controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,10 +3,7 @@ module.exports = {
     "../src/components/**/stories.tsx"
   ],
   "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-knobs'
+    "@storybook/addon-essentials"
   ],
   "framework": "@storybook/react",
   "core": {
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,30 +1,20 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
-import { ComponentMeta, Story } from '@storybook/react'
+import { Meta, Story } from '@storybook/react'
 import Main from '.'
 import React from 'react'
 
 export default {
   title: 'Main',
-  component: 'Main',
-  decorators: [withKnobs]
-} as unknown as ComponentMeta<typeof Main>
+  component: Main,
+  args: {
+    title: 'title default',
+    description: 'descriprion default'
+  }
+} as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title basic',
+  description: 'description basic'
+}
 
-export const Secondary: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado123')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Default: Story = (args) => <Main {...args} />
