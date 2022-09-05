9bc86d1e60003fcb22f1961b419d909973505404
Marcos César Domingues <marcos.domingues@gcbinvestimentos.com>
2022-09-05 10:23:14 -0300

    Configurando o PWA

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development'
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
-module.exports = nextConfig
+module.exports = withPWA({
+  // next.js config
+})
